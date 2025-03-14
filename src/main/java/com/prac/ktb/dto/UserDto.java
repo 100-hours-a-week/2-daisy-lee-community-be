15754dc852f007974e51f4a9ce0b0dd0dc750955
daisy.lee <zzeonii0612@gmail.com>
2025-03-15 01:41:13 +0900

    Add: UserRequestDto 추가

--- src/main/java/com/prac/ktb/dto/UserDto.java
+++
@@ -1,4 +0,0 @@
-package com.prac.ktb.dto;
-
-public class UserDto {
-}
---
+++ src/main/java/com/prac/ktb/dto/UserRequestDto.java
@@ -0,0 +1,19 @@
+package com.prac.ktb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDto {
+    private Long id;
+    private String password;
+    private String email;
+    private String nickname;
+    private String profileImage;
+    private Date droppedAt;
+}
