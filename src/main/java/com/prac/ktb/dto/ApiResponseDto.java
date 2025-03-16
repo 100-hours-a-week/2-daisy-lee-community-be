0c44f0b1f24ff2f3f7313dc9a7bc4a2ba9fb162e
daisy.lee <zzeonii0612@gmail.com>
2025-03-16 23:39:14 +0900

    Chore: ApiResponseDto 코드 수정

--- src/main/java/com/prac/ktb/dto/ApiResponseDto.java
+++ src/main/java/com/prac/ktb/dto/ApiResponseDto.java
@@ -1,14 +1,13 @@
 package com.prac.ktb.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-public class ApiResponseDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponseDto<T> {
     private String message;
     private Object data;
-
-    public ApiResponseDto(String message, Object data) {
-        this.message = message;
-        this.data = data;
-    }
 }
