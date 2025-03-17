6ec30b050ca37becaf31c58aff33dd4dc6b7b4ef
daisy.lee <zzeonii0612@gmail.com>
2025-03-17 11:43:17 +0900

    Refactor: properties 변경에 따른 소스 수정

--- src/main/java/com/prac/ktb/auth/JwtProvider.java
+++ src/main/java/com/prac/ktb/auth/JwtProvider.java
@@ -3,23 +3,36 @@ package com.prac.ktb.auth;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JwtProvider {
     @Value("${jwt.secret}")
-    private String SECRET_KEY;
+    private Key SECRET_KEY;
+
+    @Value("${jwt.expiration}")
+    private Long EXPIRATION_TIME;
+
+    public JwtProvider(@Value("${jwt.secret}") String secretKey,
+                       @Value("${jwt.expiration}") long expirationTime) {
+        // @Value가 기본적으로 String 타입으로 주입하기 때문에 직접 Key 객체로 변환하는 과정
+        this.SECRET_KEY = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secretKey));
+        this.EXPIRATION_TIME = expirationTime;
+    }
 
     public String generateToken(Long userId) {
         return Jwts.builder()
                 .setSubject(String.valueOf(userId))
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
 
