e24ea19165e138caf144cb2180131cbd304f96a0
daisy.lee <zzeonii0612@gmail.com>
2025-03-17 17:18:56 +0900

    Feat: login, logout 기능 추가
    - LoginResponseDto 형식 추가
    - 인증 관련 처리이므로 사용자 소스와 분리

--- src/main/java/com/prac/ktb/auth/controller/AuthController.java
+++ src/main/java/com/prac/ktb/auth/controller/AuthController.java
@@ -1,10 +1,39 @@
 package com.prac.ktb.auth.controller;
 
+import com.prac.ktb.auth.dto.LoginResponseDto;
+import com.prac.ktb.auth.service.AuthService;
+import com.prac.ktb.common.dto.ApiResponseDto;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController("/auth/sessions")
 @Transactional
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponseDto<LoginResponseDto>> login(@RequestBody String email, String password) {
+        LoginResponseDto loginResponseDto = authService.login(email, password);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponseDto<>("user_login_success", loginResponseDto));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ApiResponseDto<Map<String, Object>>> logout(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponseDto<>("user_logout_success", null));
+    }
 }
--- src/main/java/com/prac/ktb/auth/service/AuthService.java
+++ src/main/java/com/prac/ktb/auth/service/AuthService.java
@@ -1,4 +1,38 @@
 package com.prac.ktb.auth.service;
 
+import com.prac.ktb.auth.config.JwtProvider;
+import com.prac.ktb.auth.dto.LoginResponseDto;
+import com.prac.ktb.common.exception.CustomException;
+import com.prac.ktb.user.entity.User;
+import com.prac.ktb.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, JwtProvider jwtProvider, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.jwtProvider = jwtProvider;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public LoginResponseDto login(String email, String password) {
+        User loginUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException("user_login_fail", HttpStatus.NOT_FOUND));
+
+        if(!passwordEncoder.matches(password, loginUser.getPassword())) {
+            throw new CustomException("user_login_fail", HttpStatus.UNAUTHORIZED);
+        }
+
+        String jwt = jwtProvider.generateToken(loginUser.getId());
+
+        return new LoginResponseDto(loginUser.getId(), jwt);
+    }
+
+
 }
